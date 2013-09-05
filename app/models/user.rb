67163659b9083a32e64e7948ff942794fdcc6dde
Santiago Ponce <santiagoponcemoreno@hotmail.com>
2013-09-05 10:38:40 +0200

    Boostrap y Devise implementado

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+    #Validaciones
+
+    validates :password, :username, presence: true
+    validate :username, uniqueness: true
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BaseConocimiento::Application.config.secret_key_base = '9f46bc304163c9acbe55e12f9b42b995e0d506c1912973fca70306324b1c203e86759e138904b41d8ab9013ce5e8c1ccf4b91222eecc5c0c4b7a42c22dd117f8'
